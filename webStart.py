9847503cdeae11bafc490b5b5ceaed056b2376f3
root <root@DESKTOP-7U1SCBV.localdomain>
2018-01-27 15:51:48 -0600

    Added query_page, result_page, fixed landing_page.

--- webStart.py
+++ webStart.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 
 @app.route('/')
 def index():
-    return "Hello world"
+    return render_template("landing_page.html")
 
+@app.route('/itinerary')
+def itinerary():
+    return render_template("query_page.html")
+
+@app.route('/result')
+def result():
+    return render_template("result_page.html")
 
 if __name__ == "__main__":
     app.run()
