26c63c28acc71d5e3fb3d184107bd0ae361459aa
Mannan02 <31549844+Mannan02@users.noreply.github.com>
2018-01-28 02:50:34 -0600

    Update museums.py

--- museums.py
+++ museums.py
@@ -1,32 +1,31 @@
 import csv
 
+
 class museumData():
 
     def __init__(self):
+        readMyFile(self, 'museums.csv')
+
+    def readMyFile(self, filename):
+        city = []
+        museumName = []
+        state = []
 
 
-    def getMuseum(self):
+        with open(filename) as csvDataFile:
+            csvReader = csv.reader(csvDataFile)
+            for row in csvReader:
+                museumName.append(row[1])
+                city.append(row[7])
+                state.append(row[8])
+
+        return museumName, city, state
 
 
-
-
-def readMyFile(filename):
-    city = []
-    museumName = []
-    state = []
-
-    with open(filename) as csvDataFile:
-        csvReader = csv.reader(csvDataFile)
-        for row in csvReader:
-            museumName.append(row[1])
-            city.append(row[7])
-            state.append(row[8])
-
-    return museumName, city, state
-
-
-museumName, city, state= readMyFile('static/museums.csv')
-
-for i in range(0, len(city)):
-    if(city[i] == "ANCHORAGE"): #enter what the user put for their location
-        print(museumName[i])
+#museumName, city, state = readMyFile('static/museums.csv')
+    def getMuseum(cityName):
+        outputMuseum = []
+        for i in range(0, len(city)):
+            if (city[i] == cityName):  # enter what the user put for their location
+                outputMuseum.append(museumName[i])
+        return outputMuseum
