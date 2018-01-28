df5f25cf24c7c6864b9d10e3dbce733611e5946c
Mannan02 <31549844+Mannan02@users.noreply.github.com>
2018-01-28 06:41:11 -0600

    Update restaurants.py

--- restaurants.py
+++ restaurants.py
@@ -1,24 +1,26 @@
 import csv
+import random
 
 class restaurantData():
 
     def __init__(self):
-        readMyFile(self, filename)
+        
 
-    def readMyFile(self, filename):
-        city = []
-        restaurantName = []
-        #state = []
+        self.city = []
+        self.restaurantName = []
+        self.outputRestaurants = []
 
-        with open(filename) as csvDataFile:
+        with open("r.csv") as csvDataFile:
             csvReader = csv.reader(csvDataFile)
             for row in csvReader:
-                restaurantName.append(row[20])
-                city.append(row[2])
-                #state.append(row[8])
-        return restaurantName, city
+                self.restaurantName.append(row[20])
+                self.city.append(row[2])
 
-    def getRestaurant(cityName):
-        for i in range(0, len(city)):
-            if(city[i] == cityName): #enter what the user put for their location
-                return(restaurantName[i]) #returns a single restaurant, the first on the list..
+   
+
+    def getRestaurant(self,cityName):
+        for i in range(0, len(self.city)):
+            if(self.city[i] == cityName): #enter what the user put for their location
+                self.outputRestaurants.append(self.restaurantName[i])
+
+        return self.outputRestaurants[random.randint(0,len(self.outputRestaurants)) - 1];
