1baaa7aa4f68f80c47b12dfbdfd612043e1fd934
Mannan02 <31549844+Mannan02@users.noreply.github.com>
2018-01-28 06:56:18 -0600

    Update webStart.py

--- webStart.py
+++ webStart.py
@@ -3,6 +3,7 @@ app = Flask(__name__, static_url_path="/static, /static/css")
 import sys
 import restaurants.py
 import museums.py
+import parks.py
 
 @app.route('/')
 def index():
@@ -22,6 +23,17 @@ def registerForm():
     if request.method == 'POST':
         return str(request.form)
         #return str(dir(request))
+    res=restaurants.restaurantData()
+    mus=museums.museumData()
+    par=parks.parkData()
+    resArr=[]
+    musArr=[]
+    parArr=[]
+    for i in range(0,2):
+        resArr.append(res.getRestraunt(Form.get("City")).title())
+        musArr.append(mus.getMuseum(Form.get("City")).upper())
+        parArr.append(par.getPark(Form.get("State")).upper())
+
     return "Nothin'"
 if __name__ == "__main__":
     app.run()
